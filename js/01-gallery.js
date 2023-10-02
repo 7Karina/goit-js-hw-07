65747f778328e562377eba892fde07ce87d65850
@7Karina <karinafrank750@gmail.com>
2023-10-02 17:38:47 +0300

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join("");
+
+container.insertAdjacentHTML("beforeend", markup);
+
+container.addEventListener("click", handlerClick);
+
+function handlerClick(e) {
+  e.preventDefault();
+
+  const target = e.target;
+  if (target.nodeName !== "IMG") return;
+  const imageUrl = target.dataset.source;
+  const instance = basicLightbox.create(`
+      <img src="${imageUrl}" width="800" height="600">
+    `);
+  instance.show();
+  document.addEventListener("keydown", handleKeyDown);
+  function handleKeyDown(event) {
+    if (event.key !== "Escape") return;
+    instance.close();
+    document.removeEventListener("keydown", handleKeyDown);
+  }
+}
